3c389398ffea01ecff4eb83163cecc304f049e5d
Nika254 <abelcq02@gmail.com>
2025-10-17 21:23:17 +0000

    First Commit

---
+++ src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [{"label": "A dummy todo", "done": True}]
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    todos.append(request_body)
+    return jsonify(todos), 200
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    idx = position
+    if idx < 0 or idx >= len(todos):
+        return jsonify({"error": "Invalid position"}), 404
+    todos.pop(idx)
+    return jsonify(todos), 200
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
